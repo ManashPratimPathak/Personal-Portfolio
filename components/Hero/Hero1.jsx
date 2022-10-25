614208dff65dbc9d7393c23558f07cf36b0de89c
Manash Pratim Pathak <mppoff021@gmail.com>
2022-10-25 18:07:46 +0530

    tried different images but not Satisfied

--- components/Hero/Hero1.jsx
+++ components/Hero/Hero1.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import style from "./Hero.module.css";
 import Button1 from '../Shared/Button1';
+import Image from 'next/image'
+import bg1 from '../../public/images/1.png'
+import bg2 from '../../public/images/2.svg'
 
 
 const fontStyle = {
-    color: "#fff",
+    color: "#7a52a4",
     fontSize: "1rem",
     fontFamily: "'Julius Sans One', sans-serif"
   };
@@ -12,9 +15,19 @@ const fontStyle = {
 function Hero1() {
   return (
     <>
-        <div className='w-full h-screen flex flex-col justify-center items-center'>
+        <div className='w-full h-screen flex flex-row bg-white'>
+        <div className='w-[60vw] h-screen flex flex-col justify-center'>
+        <h1 className={style.helloText} style={{color: "#7a52a4", fontSize: "3.5rem", fontFamily: "'Julius Sans One', sans-serif", textAlign: "left "}} >Manash Pratim Pathak</h1>
             <p className={style.helloText} style={fontStyle} >Web Developer | Technical Head @ E-Cell NIT Silchar | EE NITS'24</p>
             <Button1 name="Explore more" bcolor="whites" href="#AboutMe"/>
+        </div>
+        <div className=' relative w-[20vw] h-screen flex flex-col justify-center items-center'>
+          <Image src={bg2} layout="fill"/>
+        </div>
+        <div className=' relative w-[20vw] h-screen flex flex-col justify-center items-center'>
+          <Image src={bg1} layout="fill"/>
+        </div>
+            
         </div>
     </>
   )
